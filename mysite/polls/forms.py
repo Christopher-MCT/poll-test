66fd6330af6d8b6c8f2414798d2ef68d818c4d67
Christopher Calix <ccalix@residentes1.oficina.yezdo.com>
2023-09-13 17:58:36 -0600

    Actualizacion3

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,5 +1,5 @@
 from django import forms    
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, BaseUserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -24,4 +24,24 @@ class SignupForm(UserCreationForm):
 
 
 
-    
+class UserCreationForm(BaseUserCreationForm):
+    def clean_username(self):
+        """Reject usernames that differ only in case."""
+        username = self.cleaned_data.get("username")
+        if (
+            username
+            and self._meta.model.objects.filter(username__iexact=username).exists()
+        ):
+            self._update_errors(
+                ValidationError(
+                    {
+                        "username": self.instance.unique_error_message(
+                            self._meta.model, ["username"]
+                        )
+                    }
+                )
+            )
+        else:
+            return username
+        
+# """ https://github.com/django/django/blob/main/django/contrib/auth/forms.py#L152"""
